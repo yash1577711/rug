b937606fa92afff3d0050bcba91fb950cbc4b03f
yash1577711 <151181767+yash1577711@users.noreply.github.com>
2026-01-29 13:05:02 +0530

    Update urls.py

--- rugshop/urls.py
+++ rugshop/urls.py
@@ -14,8 +14,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('create-admin/', core_views.create_admin_user, name='create_admin'),
     path('', core_views.home, name='home'),
-    path('products/', include('products.urls')),  # ← This now works!
-
+    path('products/', include('products.urls')),  
     path('orders/', include('orders.urls')),
     path('trade/', core_views.trade_view, name='trade'),
     path('owner/', core_views.owner_dashboard, name='owner_dashboard'),
@@ -26,11 +25,8 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('about/', core_views.about, name='about'),
     path('set-ip/', core_views.set_manual_ip, name='set_manual_ip'),
- 
     path('set-currency/', core_views.set_currency, name='set_currency'),
     path('api/currency-info/', core_views.get_currency_info, name='currency_info'),
-
-
     path('success/', views.order_success, name='order_success'),
     path('cancel/', views.order_cancel, name='order_cancel'),
     path('my-orders/', views.user_orders, name='user_orders'),
@@ -40,7 +36,3 @@ urlpatterns = [
     
 ]
 
-# ONLY FOR DEVELOPMENT (when DEBUG=True)
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
