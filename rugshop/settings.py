e5210e9bc860b1efa3dadfcd7ccba2e7719b9d5d
yash1577711 <151181767+yash1577711@users.noreply.github.com>
2026-01-29 18:59:06 +0530

    Update settings.py

--- rugshop/settings.py
+++ rugshop/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'accounts',
     'orders',
 ]
-
+SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
