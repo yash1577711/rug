e325800ee6051f55296bfbebf23d174f95deeabe
yash1577711 <151181767+yash1577711@users.noreply.github.com>
2026-02-10 05:50:52 +0530

    Fix allowed host URL in settings.py

--- rugshop/settings.py
+++ rugshop/settings.py
@@ -20,7 +20,7 @@ DEBUG = env('DEBUG')
 ALLOWED_HOSTS = [
     "considerate-transformation-production-fd62.up.railway.app",
     "localhost",
-   " https://kalinbaba.onrender.com",
+   "kalinbaba.onrender.com",
     "127.0.0.1",
 ]
 
